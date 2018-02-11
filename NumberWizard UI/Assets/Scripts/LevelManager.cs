e0df00bc201f0156dde5e8a9bab80e717dc46e7c
Jason Whiting <Jasons.o.g.a@gmail.com>
2018-02-11 16:39:01 -0500

    Uploaded "NumberWizard UI" Unity Files.

---
+++ NumberWizard UI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string level) {
+		Debug.Log("Level Load Requested for " + level);
+		Application.LoadLevel(level);
+	}
+	
+	public void Quit() {
+		Debug.Log("Quit Requested.");
+		Application.Quit();
+	}
+	
+}
---
+++ NumberWizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	
+	public Text text;
+	public int count;
+	public int maxGuesses = 10;
+	
+	int max;
+	int min;
+	int guess;
+	
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame() {
+		
+		max = 10000;
+		min = 1;
+		guess = Random.Range(min, max);
+		//guess = (max + min) / 2;
+		
+		text.text = "Is your number: " + guess + "?";
+		
+		count = 1;
+		
+	}
+	
+	public void GuessHigher() {
+		
+		min = guess;
+		NextGuess();
+		
+	}
+	
+	public void GuessLower() {
+		
+		max = guess;
+		NextGuess();
+		
+	}
+	
+	void NextGuess() {
+		
+		guess = Random.Range(min, max);
+		// guess = (max + min) / 2
+		
+		text.text = "Is your number: " + guess + "?";
+		
+		maxGuesses = maxGuesses - 1;
+		if (maxGuesses <= 0) {
+			Application.LoadLevel("Win");
+		}
+		
+		count = count + 1;
+		
+	}
+	
+}
